3b742e48337451c5b20581f62a7fb7f1277b8380
ArjunTechie118 <arjun.dadhich004@gmail.com>
2026-09-10 05:45:24 +0530

    Saved final model

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
-import streamlit
-import pandas as pd
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from typing import Literal,Optional
+from pydantic import BaseModel,Field,computed_field
 import joblib
+
+#load model
+model = joblib.load("src/churn_xgb_pipeline.pkl")
+
+app = FastAPI()
+
